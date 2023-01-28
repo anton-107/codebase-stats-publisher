e9b138267addbcbd669666c46764cc31e9682017
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-01-28 18:40:21 +0100

    publish files as plaintext notes via notes api client

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,31 @@
 import {access} from 'fs';
 import { promisify } from 'util';
+import {GitRepository} from "codebase-stats-collector/dist/git-reader/git-repository.js";
+import {getNumberOfChangesPerFile} from 'codebase-stats-collector/dist/stats/number-of-changes-per-file.js';
+import {getNumberOfLines} from 'codebase-stats-collector/dist/stats/number-of-lines.js';
+import {getNumberOfContributorsPerFile} from 'codebase-stats-collector/dist/stats/number-of-contributors-per-file.js';
+import {APIClient} from "notes-webserver-apiclient/dist/api-client.js";
 
 const checkFileExists = promisify(access);
 
+interface File {
+  filePath: string;
+  numberOfLines: number;
+  numberOfChanges: number;
+  numberOfContributors: number;
+}
+
 async function main() {
-  const {GitRepository} = await import('codebase-stats-collector/dist/git-reader/git-repository.js');
-  const {getNumberOfChangesPerFile} = await import('codebase-stats-collector/dist/stats/number-of-changes-per-file.js');
-  const {getNumberOfLines} = await import('codebase-stats-collector/dist/stats/number-of-lines.js');
-  const {getNumberOfContributorsPerFile} = await import('codebase-stats-collector/dist/stats/number-of-contributors-per-file.js');
 
   const gitRepo = process.env.SOURCE_DIR;
-  const notesEndpoint = process.env.NOTES_API_ENDPOINT;
+  const notesEndpoint = process.env.API_ROOT;
   const notesUser = process.env.NOTES_USER;
   const notesPassword = process.env.NOTES_PASSWORD;
   if (!gitRepo) {
     throw Error('Please define SOURCE_DIR to point to your local git repository to collect data from');
   }
   if (!notesEndpoint) {
-    throw Error('Please define NOTES_API_ENDPOINT to point to your local or remote Notes API');
+    throw Error('Please define API_ROOT to point to your local or remote Notes API');
   }
   if (!notesUser) {
     throw Error('Please define NOTES_USER to specify the Notes user to publish the repo data');
@@ -36,6 +44,8 @@ async function main() {
   const numberOfChangesPerFile = await getNumberOfChangesPerFile(commitsWithChangedFiles);
   const numberOfContributorsPerFile = await getNumberOfContributorsPerFile(commitsWithChangedFiles);
   const filePaths = Object.keys(numberOfChangesPerFile);
+
+  const files: File[] = [];
   for (let filePath of filePaths) {
     const fullPath = `${gitRepo}/${filePath}`;
 
@@ -51,9 +61,27 @@ async function main() {
     const numberOfChanges = numberOfChangesPerFile[filePath];
     const numberOfContributors = numberOfContributorsPerFile[filePath];
     console.log(filePath, numberOfLines, numberOfChanges, numberOfContributors);
+    files.push({filePath, numberOfLines, numberOfChanges, numberOfContributors});
   }
 
   // publish data:
   console.log(`Publishing collected data to: ${notesEndpoint} as user ${notesUser}`);
+
+  const client = new APIClient();
+  const isAuthorized = await client.authorize(notesUser, notesPassword);
+  if (!isAuthorized) {
+    throw Error(`Could not authorize Notes API client. Check that you are using correct login/password pair`);
+  }
+  console.log('Client is authorized');
+  const notebookResponse = await client.createNotebook(`Git repo (test)`);
+  if (notebookResponse.httpCode !== 200) {
+    throw Error(`Could not create notebook: ${notebookResponse.httpCode} - ${notebookResponse.body}`);
+  }
+  let i = 0;
+  for (let file of files) {
+    const noteResponse = await client.createNote(notebookResponse.body.id, JSON.stringify(file));
+    i += 1;
+    console.log(`Response from note creation ${file.filePath}: ${noteResponse.httpCode}. Progress: ${i} out of ${files.length} complete`);
+  }
 }
 main();
