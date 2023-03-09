107db72f0253712b7cfecde96001f9ed06892002
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-03-09 22:29:35 +0100

    add check-format, check-dependencies and check-duplications build steps

--- src/collect-data.ts
+++ src/collect-data.ts
@@ -1,9 +1,9 @@
-import {GitRepository} from "codebase-stats-collector/dist/git-reader/git-repository.js";
-import {getNumberOfChangesPerFile} from 'codebase-stats-collector/dist/stats/number-of-changes-per-file.js';
-import {getNumberOfContributorsPerFile} from 'codebase-stats-collector/dist/stats/number-of-contributors-per-file.js';
-import {getNumberOfLines} from 'codebase-stats-collector/dist/stats/number-of-lines.js';
-import {access} from 'fs';
-import { promisify } from 'util';
+import { GitRepository } from "codebase-stats-collector/dist/git-reader/git-repository.js";
+import { getNumberOfChangesPerFile } from "codebase-stats-collector/dist/stats/number-of-changes-per-file.js";
+import { getNumberOfContributorsPerFile } from "codebase-stats-collector/dist/stats/number-of-contributors-per-file.js";
+import { getNumberOfLines } from "codebase-stats-collector/dist/stats/number-of-lines.js";
+import { access } from "fs";
+import { promisify } from "util";
 
 const checkFileExists = promisify(access);
 
@@ -18,31 +18,39 @@ export async function collectDataFromGitRepo(gitRepo: string): Promise<File[]> {
   // read data from git:
   const repo = new GitRepository(gitRepo);
   const commitsWithChangedFiles = await repo.getListOfCommitsWithChangedFiles();
-  
+
   // analyze stats:
-  const numberOfChangesPerFile = await getNumberOfChangesPerFile(commitsWithChangedFiles, {
-    fileIgnorePattern: `^/dist/`
-  });
-  const numberOfContributorsPerFile = await getNumberOfContributorsPerFile(commitsWithChangedFiles);
+  const numberOfChangesPerFile = await getNumberOfChangesPerFile(
+    commitsWithChangedFiles,
+    {
+      fileIgnorePattern: `^/dist/`,
+    }
+  );
+  const numberOfContributorsPerFile = await getNumberOfContributorsPerFile(
+    commitsWithChangedFiles
+  );
   const filePaths = Object.keys(numberOfChangesPerFile);
 
   const files: File[] = [];
   for (const filePath of filePaths) {
     const fullPath = `${gitRepo}/${filePath}`;
 
-    
-    
     let numberOfLines = null;
     try {
       await checkFileExists(fullPath);
       numberOfLines = await getNumberOfLines(fullPath);
     } catch {
-      console.warn('file does not exist:', fullPath) 
+      console.warn("file does not exist:", fullPath);
     }
     const numberOfChanges = numberOfChangesPerFile[filePath];
     const numberOfContributors = numberOfContributorsPerFile[filePath];
-    files.push({filePath, numberOfLines, numberOfChanges, numberOfContributors});
+    files.push({
+      filePath,
+      numberOfLines,
+      numberOfChanges,
+      numberOfContributors,
+    });
   }
-  
+
   return files;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {APIClient} from "notes-webserver-apiclient/dist/api-client.js";
+import { APIClient } from "notes-webserver-apiclient/dist/api-client.js";
 
 import { collectDataFromGitRepo, File } from "./collect-data.js";
 
@@ -15,61 +15,71 @@ function configureFromEnvironment(): PublisherConfiguration {
   const notesUser = process.env.NOTES_USER;
   const notesPassword = process.env.NOTES_PASSWORD;
   if (!gitRepo) {
-    throw Error('Please define SOURCE_DIR to point to your local git repository to collect data from');
+    throw Error(
+      "Please define SOURCE_DIR to point to your local git repository to collect data from"
+    );
   }
   if (!notesEndpoint) {
-    throw Error('Please define API_ROOT to point to your local or remote Notes API');
+    throw Error(
+      "Please define API_ROOT to point to your local or remote Notes API"
+    );
   }
   if (!notesUser) {
-    throw Error('Please define NOTES_USER to specify the Notes user to publish the repo data');
+    throw Error(
+      "Please define NOTES_USER to specify the Notes user to publish the repo data"
+    );
   }
   if (!notesPassword) {
-    throw Error('Please define NOTES_PASSWORD to specify the Notes user password');
+    throw Error(
+      "Please define NOTES_PASSWORD to specify the Notes user password"
+    );
   }
   return {
     gitRepo,
     notesEndpoint,
     notesUser,
-    notesPassword
+    notesPassword,
   };
 }
 
 function buildNote(notebookID: string, file: File) {
   return {
-    id: '',
-    'note-type': 'source-file',
+    id: "",
+    "note-type": "source-file",
     notebookID,
     content: file.filePath,
-    'number-of-lines': String(file.numberOfLines),
-    'number-of-changes': String(file.numberOfChanges),
-    'number-of-contributors': String(file.numberOfContributors)
+    "number-of-lines": String(file.numberOfLines),
+    "number-of-changes": String(file.numberOfChanges),
+    "number-of-contributors": String(file.numberOfContributors),
   };
 }
 
 async function main() {
-  const {
-    gitRepo,
-    notesEndpoint,
-    notesUser,
-    notesPassword
-  } = configureFromEnvironment();
+  const { gitRepo, notesEndpoint, notesUser, notesPassword } =
+    configureFromEnvironment();
 
   console.log(`Collecting data from git repo: ${gitRepo}`);
 
   const files = await collectDataFromGitRepo(gitRepo);
 
   // publish data:
-  console.log(`Publishing collected data to: ${notesEndpoint} as user ${notesUser}`);
+  console.log(
+    `Publishing collected data to: ${notesEndpoint} as user ${notesUser}`
+  );
 
   const client = new APIClient();
   const isAuthorized = await client.authorize(notesUser, notesPassword);
   if (!isAuthorized) {
-    throw Error(`Could not authorize Notes API client. Check that you are using correct login/password pair`);
+    throw Error(
+      `Could not authorize Notes API client. Check that you are using correct login/password pair`
+    );
   }
-  console.log('Client is authorized');
+  console.log("Client is authorized");
   const notebookResponse = await client.createNotebook(`Git repo (test v2)`);
   if (notebookResponse.httpCode !== 200) {
-    throw Error(`Could not create notebook: ${notebookResponse.httpCode} - ${notebookResponse.body}`);
+    throw Error(
+      `Could not create notebook: ${notebookResponse.httpCode} - ${notebookResponse.body}`
+    );
   }
 
   const publishBatchSize = 10;
@@ -79,17 +89,23 @@ async function main() {
     i += 1;
     notes.push(buildNote(notebookResponse.body.id, file));
     if (notes.length < publishBatchSize) {
-      console.log(`Collected note data on ${file.filePath} to publish in batch of ${publishBatchSize}. Progress: ${i} out of ${files.length} processed`);
+      console.log(
+        `Collected note data on ${file.filePath} to publish in batch of ${publishBatchSize}. Progress: ${i} out of ${files.length} processed`
+      );
       continue;
     }
 
     const noteResponse = await client.createNotes(notes);
     notes = [];
-    console.log(`Response from note creation : ${noteResponse.httpCode}. Progress: ${i} out of ${files.length} complete`);
+    console.log(
+      `Response from note creation : ${noteResponse.httpCode}. Progress: ${i} out of ${files.length} complete`
+    );
   }
   // publish last batch:
   const noteResponse = await client.createNotes(notes);
   notes = [];
-  console.log(`Response from note creation : ${noteResponse.httpCode}. Progress: ${i} out of ${files.length} complete`);
+  console.log(
+    `Response from note creation : ${noteResponse.httpCode}. Progress: ${i} out of ${files.length} complete`
+  );
 }
 main();
